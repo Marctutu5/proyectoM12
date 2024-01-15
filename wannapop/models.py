6087604efb1198d0405e5ea6afce6faa7fbafd38
Its_Draky <axviro@fp.insjoaquimmir.cat>
2024-01-15 17:25:50 +0100

    Agregar mixins y actualizar modelos

--- wannapop/models.py
+++ wannapop/models.py
@@ -1,9 +1,10 @@
 from . import db_manager as db
+from .mixins import BaseMixin, SerializableMixin
 from sqlalchemy.sql import func
 from flask_login import UserMixin
 from datetime import datetime
 
-class Product(db.Model):
+class Product(BaseMixin, SerializableMixin, db.Model):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -15,13 +16,13 @@ class Product(db.Model):
     created = db.Column(db.DateTime, server_default=func.now())
     updated = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
-class Category(db.Model):
+class Category(BaseMixin, SerializableMixin, db.Model):
     __tablename__ = "categories"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     slug = db.Column(db.String, nullable=False)
 
-class User(UserMixin, db.Model):
+class User(BaseMixin, SerializableMixin, UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True, nullable=True)
@@ -36,13 +37,13 @@ class User(UserMixin, db.Model):
     def get_id(self):
         return self.email
 
-class BlockedUser(db.Model):
+class BlockedUser(BaseMixin, SerializableMixin, db.Model):
     __tablename__ = "blocked_users"
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     message = db.Column(db.String(255))
     created = db.Column(db.DateTime, default=datetime.utcnow)
 
-class BannedProduct(db.Model):
+class BannedProduct(BaseMixin, SerializableMixin, db.Model):
     __tablename__ = 'banned_products'
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
     reason = db.Column(db.String(255))
