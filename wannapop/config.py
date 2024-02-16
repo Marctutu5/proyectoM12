df1c24eac90bd2164766fcfb7f834125413bfe42
Marctutu5 <marctutu5@gmail.com>
2024-02-16 17:30:38 +0100

    He arribat fins aqui, hi ha algun problema en la connexió entre flask i nginx 1.0.5.3-docker_flask

--- wannapop/config.py
+++ wannapop/config.py
@@ -1,36 +1,16 @@
 import os
 from os import environ
+SECRET_KEY = os.environ.get("SECRET_KEY", "default-secret-key")
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SQLALCHEMY_ECHO = os.environ.get("SQLALCHEMY_ECHO", "False").lower() in ['true', '1', 't']
+DEBUG = os.environ.get("FLASK_ENV") == "development"
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG').upper()
 
-# Secret key
-SECRET_KEY = "Valor aleatori molt llarg i super secret"
+MAIL_SENDER_NAME = os.environ.get("MAIL_SENDER_NAME")
+MAIL_SENDER_ADDR = os.environ.get("MAIL_SENDER_ADDR")
+MAIL_SENDER_PASSWORD = os.environ.get("MAIL_SENDER_PASSWORD")
+MAIL_SMTP_SERVER = os.environ.get("MAIL_SMTP_SERVER")
+MAIL_SMTP_PORT = os.environ.get("MAIL_SMTP_PORT")
 
-# ruta absoluta d'aquesta carpeta
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-
-# postgres amb docker-compose
-SQLALCHEMY_DATABASE_URI = "postgresql://user:patata@127.0.0.1:5432/userdb"
-
-# paràmetre que farà servir SQLAlchemy per a connectar-se
-# SQLALCHEMY_DATABASE_URI = "postgresql://2dd09:0vOxXzRcjty38wSP@37.27.3.70:5432/2dd09_pg"
-
-# mostre als logs les ordres SQL que s'executen
-SQLALCHEMY_ECHO = True
-
-SECRET_KEY="Valor aleatori molt llarg i super secret"
-# SQLITE_FILE_RELATIVE_PATH="sqlite/database.db"
-
-MAIL_SENDER_NAME="Administrador"
-MAIL_SENDER_ADDR="2daw.equip09@fp.insjoaquimmir.cat"
-MAIL_SENDER_PASSWORD="BCaIo5785@[="
-MAIL_SMTP_SERVER="smtp.gmail.com"
-MAIL_SMTP_PORT="587"
-
-CONTACT_ADDR="matuvi@fp.insjoaquimmir.cat"
-EXTERNAL_URL="http://127.0.0.1:5000"
-
-# DEBUG TOOLBAR
-DEBUG = True
-DEBUG_TB_INTERCEPT_REDIRECTS = False
-
-# Configuración del nivel de registro
-LOG_LEVEL = environ.get('LOG_LEVEL', 'DEBUG').upper()
+CONTACT_ADDR = os.environ.get("CONTACT_ADDR")
+EXTERNAL_URL = os.environ.get("EXTERNAL_URL")
--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,7 @@ from wannapop import create_app
 
 create_app()
 
+# if __name__ == "__main__":
+#     app.run(host="127.0.0.1", port=5000, debug=True)
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)  # Aunque esta línea no se usará con Gunicorn, es buena práctica definirla.
